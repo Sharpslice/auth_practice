12853949d7fc9d607bff28c58f8c7abae2c70221
sharpslice <davidlein1440p@gmail.com>
2025-07-17 13:24:12 -0700

    feat: set up user authentication middleware

--- app.js
+++ app.js
@@ -42,4 +42,25 @@ app.post('/sign-up',async(req,res,next)=>{
     
 })
 
+passport.use(new localStrategy(async(username,password,done)=>{
+    try{
+        const {rows} = await pool.query(`
+            SELECT * FROM users
+            WHERE username = $1 
+        `,[username])
+        const user = rows[0];
+        if(!user){
+            return done(null,false,{message: 'Incorrect Username'})
+        }
+        if(user.password !== password){
+            return done(null,false,{message: 'Incorrect Password'});
+        }
+        return done(null,user)
+    }catch(err){
+        return done(err)
+    }
+
+
+}));
+
 app.listen(3000,()=> console.log("app listening on port 3000!"))
